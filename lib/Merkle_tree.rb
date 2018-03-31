96a0763c10a28872099d4ae65c6dc000d0c9b8de
康峥非 <kangzhengfei@tanmer.com>
2018-03-31 10:08:02 +0800

    add something for encryption

---
+++ lib/Merkle_tree.rb
@@ -0,0 +1,12 @@
+# Data structure: tree
+# The leaf value is the hash value of node data
+# The child value is the hash value of its children
+# ======================================================
+# Example: Digest::SHA1.hexdigest 'message'
+require 'digest'
+
+class MerkleTree
+  def initialize()
+
+  end
+end
